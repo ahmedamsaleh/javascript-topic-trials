4f8c799910894c1ba94f6b26ab0d53f3555f4b58
jhtrujil <jhtrujil@us.ibm.com>
2017-02-14 17:15:05 -0500

    pass first ispalindrome test

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,4 +1,4 @@
-describe.only('the fahrenheit2celsius canary test', () => {
+describe('the fahrenheit2celsius canary test', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,10 +1,22 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
+  function isPalindrome() {
+    return true;
+  }
+
   describe('palindrome should be', () => {
-    it('true for mom');
+    it('true for mom', () => {
+      isPalindrome('mom').should.be.true();
+    });
     it('false for dude');
+    it('true for mom dad mom');
+    it('false for dude hi');
+    it('true for whitespaces');
+    it('error for empty');
+    it('error for null');
+    it('error for noInput');
   });
 });
