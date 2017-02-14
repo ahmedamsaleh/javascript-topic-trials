c828d97e9c825f44c75cb2559c6d36584752d582
jhtrujil <jhtrujil@us.ibm.com>
2017-02-14 16:44:22 -0500

    make fahrenheit testbar green

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,16 @@
-describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+describe.only('the fahrenheit2celsius canary test', () => {
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  function fahrenheit2celsius() {
+    return 0;
+  }
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
+    it('give 0°C for 32°F', () =>{
+      fahrenheit2celsius(32).should.equal(0);
+    });
     it('give 10°C for 50°F');
   });
 });
