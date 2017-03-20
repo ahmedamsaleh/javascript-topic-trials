d4d3ab3bf0bdca0ba97b8d0013500751182aa0ad
omar-mesheal <omar.mesheal@gmail.com>
2017-03-20 16:18:31 +0200

    fahrenheit -2-celsius

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,19 @@
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  it('shows the infrastructure works', ()=>{
+    true.should.be.true();
+  });
 
-  describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+  function fahrenheitToCelsius(fahrenheit){
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+  describe('fahrenheitToCelsius should', () => {
+    it('give 0°C for 32°F', ()=>{
+      fahrenheitToCelsius(32).should.be.equal(0);
+    });
+
+    it('give 10°C for 50°F', ()=>{
+      fahrenheitToCelsius(50).should.be.equal(10);
+    });
   });
 });
