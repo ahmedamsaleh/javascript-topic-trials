92dd1390306cd699ffaaaf4e8101c0809b21d3c2
ahmedasaleh <ahmedaly@tedata.net.eg>
2017-03-21 16:50:00 +0200

    created test cases for stack

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -4,5 +4,16 @@ describe('the stack spec', () => {
   });
 
   describe('a stack should', () => {
+    it('start with size 0');
+    it('start empty');
+    it('not be empty after push');
+    it('have size of 1 after push');
+    it('be empty after push then pop');
+    it('have size of 0 after push then pop');
+
+    it('throws "empty stack error" ');
+    it('throws "full stack error" ');
+    it('pop same element after pushing it');
+    it('pop last 2 after pushing 3 times');
   });
 });
