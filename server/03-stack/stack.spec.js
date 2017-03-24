b79fec72f2a36b9af227678ef9db8f1ac34d473f
jhtrujil <jhtrujil@us.ibm.com>
2017-03-24 10:16:24 -0400

    first stack implementation

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,6 +1,6 @@
 import R from 'ramda';
 
-describe.only('the stack spec', () => {
+describe('the stack spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
