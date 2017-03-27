fba0abaa8474bac8b6ad679a10985a7b510e0083
omar-mesheal <omar.mesheal@gmail.com>
2017-03-27 16:48:10 +0200

    prime factors

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,16 @@
-describe('the prime numbers canary spec', () => {
+describe('the prime factors canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  describe.only('a prime factors function should return', () => {
+    it('no prime factor for 1');
+    it('2 for 2');
+    it('3 for 3');
+    it('2 ,2  for 4');
+    it('5 for 5');
+    it('2,3 for 6');
+    it('2,2,2 for 8');
+    it('3,3 for 9');
   });
 });
