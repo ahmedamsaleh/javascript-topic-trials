71aa42d0dc74cbe4d8779bb09ec9b8163e8f7de7
omar-meshaal <omar.meshaal@gmail.com>
2017-03-27 18:35:20 +0200

    added 2,2 for 4 test for prime factors

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,3 +1,5 @@
+/*eslint no-param-reassign: "off" */
+
 describe('the prime factors canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
@@ -5,8 +7,13 @@ describe('the prime factors canary spec', () => {
 
   let primeFactors = function (number) {
     const factors = [];
-    if (number > 1) factors.push(number);
-
+    if (number > 1) {
+      if (number % 2 === 0) {
+        factors.push(2);
+        number /= 2;
+      }
+      if (number > 1) factors.push(number);
+    }
     return factors;
   };
 
@@ -23,7 +30,10 @@ describe('the prime factors canary spec', () => {
       primeFactors(3).should.be.deepEqual([3]);
     });
 
-    it('2 ,2  for 4');
+    it('2 ,2  for 4', () => {
+      primeFactors(4).should.be.deepEqual([2, 2]);
+    });
+
     it('5 for 5');
     it('2,3 for 6');
     it('2,2,2 for 8');
