3cb6016ca11b68ec692728bebc1b212ce00d8039
omar-mesheal <omar.mesheal@gmail.com>
2017-03-27 17:03:53 +0200

    added one for one test for prime factors

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -3,9 +3,15 @@ describe('the prime factors canary spec', () => {
     true.should.be.true();
   });
 
+  let primeFactors = function () {
+    return [];
+  };
+
   describe.only('a prime factors function should return', () => {
-    it('no prime factor for 1');
-    
+    it('no prime factor for 1', () => {
+      primeFactors(1).should.be.deepEqual([]);
+    });
+
     it('2 for 2');
     it('3 for 3');
     it('2 ,2  for 4');
