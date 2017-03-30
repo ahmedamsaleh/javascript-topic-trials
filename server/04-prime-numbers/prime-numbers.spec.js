6f6c6ee51c93df9c50dfc7e741e07d0f049be8ec
jhtrujil <jhtrujil@us.ibm.com>
2017-03-30 12:39:40 -0400

    add test for 3 and add factorlist variable in prime factors

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -4,8 +4,9 @@ describe('the prime factors canary spec', () => {
   });
 
   const primeFactors = (number) => {
-    if (number === 2) return [2];
-    return [];
+    let factorList = [];
+    if (number > 1) factorList.push(number);
+    return factorList;
   };
 
   describe.only('a prime factors function should', () => {
@@ -17,7 +18,10 @@ describe('the prime factors canary spec', () => {
       primeFactors(2).should.deepEqual([2]);
     });
 
-    it('return 3 for 3');
+    it('return 3 for 3', () =>{
+      primeFactors(3).should.deepEqual([3]);
+    });
+
     it('return 2 and 2 for 4');
     it('return 5 for 5');
     it('return 2 and 3 for 6');
