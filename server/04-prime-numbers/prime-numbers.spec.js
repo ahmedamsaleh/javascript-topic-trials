3e62a6824c4bb209b7119dc4d8e721a7add2b911
omar-meshaal <omar.meshaal@gmail.com>
2017-03-30 16:41:12 +0200

    refactored prime factor by removing {}

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -8,9 +8,7 @@ describe('the prime factors canary spec', () => {
   let primeFactors = function (number) {
     const factors = [];
     for (let divisor = 2; number > 1; divisor++) {
-      for (;number % divisor === 0; number /= divisor) {
-        factors.push(divisor);
-      }
+      for (;number % divisor === 0; number /= divisor) factors.push(divisor);
     }
     return factors;
   };
