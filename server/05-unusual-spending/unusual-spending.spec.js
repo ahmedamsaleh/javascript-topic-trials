dc01faee362a57e1b84aa1bb1a2fabf59c9f5641
omar-meshaal <omar.meshaal@gmail.com>
2017-03-30 17:02:28 +0200

    unusual spending initial test

---
+++ server/05-unusual-spending/unusual-spending.spec.js
@@ -0,0 +1,9 @@
+describe.only('the unusual spending spec', () => {
+  it('shows the unusual spending works', () => {
+    true.should.be.true();
+  });
+  describe('unusual spending', () => {
+    it('should call fetch then categorize then email');
+
+  });
+});
