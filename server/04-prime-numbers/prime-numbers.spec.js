cd3afaf40a7f07476723de45b0a01815e7b22f45
jhtrujil <jhtrujil@us.ibm.com>
2017-03-30 12:10:39 -0400

    add prime number test list

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,17 @@
-describe('the prime numbers canary spec', () => {
+describe('the prime factors canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  describe.only('a prime factors function should', () => {
+    it('return none for 1');
+    it('return 2 for 2');
+    it('return 3 for 3');
+    it('return 2 and 2 for 4');
+    it('return 5 for 5');
+    it('return 2 and 3 for 6');
+    it('return 7 for 7');
+    it('return 2 and 2 and 2 for 8');
+    it('return 3 and 3 for 9');
   });
 });
