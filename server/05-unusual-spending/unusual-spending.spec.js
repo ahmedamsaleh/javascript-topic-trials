7ca3a3e021e8906ea4029d8b9479487d771f20bb
jhtrujil <jhtrujil@us.ibm.com>
2017-04-05 11:35:42 -0400

    add canary test and first test for unusual spending

---
+++ server/05-unusual-spending/unusual-spending.spec.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jhtrujil on 4/5/2017.
+ */
+import {replace, when, verify, reset} from 'td';
+
+describe.only('canary test', () => {
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  describe('unusual spending should', () => {
+    it('interact with fetch, categorize, and email', () => {});
+    afterEach(() => {
+      reset();
+    });
+  });
+});
+
+
