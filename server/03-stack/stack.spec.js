cdd3eefebc623facacca3d4f3e232c6700d705fb
Matt Oberlin <moberlin8@gmail.com>
2017-04-05 17:29:25 -0400

    First test green
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,8 +1,25 @@
-describe('the stack spec', () => {
+describe.only('the stack spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
+   let stack = {
+     isEmpty: () => true
+   };
+
   describe('a stack should', () => {
+    it('be empty on create', () => {
+      stack.isEmpty().should.be.true();
+    });
+
+    it('size one on push');
+    it('not be empty on push');
+    it('have size zero on push and pop');
+    it('empty on push and pop');
+    it('overflow');
+    it('underflow');
+    it('pop what was pushed');
+    it('pop the two elements that were pushed');
+    it('handle negative size');
   });
 });
