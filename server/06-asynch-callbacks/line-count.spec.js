1e86a1b9bfd6f0093a108eac90cd103dce2d1b2e
WIL PANNELL <wgpannel@us.ibm.com>
2017-04-06 06:51:12 -0700

    fail linecount callback success spec

--- server/06-asynch-callbacks/line-count.spec.js
+++ server/06-asynch-callbacks/line-count.spec.js
@@ -1,5 +1,15 @@
+import {lineCount} from './line-count';
+
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+  it('for file —— line-count.js —— should be 13', done => {
+    const successHandler = numberOfLines => {
+      numberOfLines.should.equal(12);
+      done();
+    };
+
+    lineCount('server/06-asynch-callbacks/line-count.js',
+        null, successHandler);
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
