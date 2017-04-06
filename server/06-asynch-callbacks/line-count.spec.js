6aad08407b0fd9258d0f54478ae25f9fa183f1ed
omar-meshaal <omar.meshaal@gmail.com>
2017-04-06 16:37:56 +0200

    introducing callbacks

--- server/06-asynch-callbacks/line-count.spec.js
+++ server/06-asynch-callbacks/line-count.spec.js
@@ -1,6 +1,22 @@
-describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+import {lineCount} from './line-Count';
 
-  it('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+describe.only('line count', () => {
+  it('for file —— line-count.js —— should be 13', (done) => {
+    const successCallback = (numberOfLines) => {
+      numberOfLines.should.be.equal(13);
+      done();
+    };
+
+    lineCount('server/06-asynch-callbacks/line-count.js', null, successCallback);
+  });
+
+  it('for file —— is-not-there.js —— should be problem reading file: ' + 'is-not-there.js', (done) => {
+
+    const errorCallback = (errorMsg) => {
+      errorMsg.should.be.equal('problem reading file: ' + 'is-not-there.js');
+      done();
+    };
+
+    lineCount('is-not-there.js', errorCallback, null);
+  });
 });
