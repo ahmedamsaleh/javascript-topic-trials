6a65c7ecf54a7e11b41629bac23a45c98c4901c2
omar-meshaal <omar.meshaal@gmail.com>
2017-04-10 16:06:02 +0200

    asyn line count

--- server/07-asynch-promises/line-count.spec.js
+++ server/07-asynch-promises/line-count.spec.js
@@ -10,15 +10,33 @@ describe.only('line count can be checked', () => {
   });
 
   it('in case of failure with the done parameter like with a callback', (done) => {
-      const errorCallback = (error) => {
-        error.message.should.be.equal('problem reading file: is-not-there.js');
-        done();
-      };
+    const errorCallback = (error) => {
+      error.message.should.be.equal('problem reading file: is-not-there.js');
+      done();
+    };
 
-      lineCount('is-not-there.js').catch(errorCallback);
+    lineCount('is-not-there.js').catch(errorCallback);
   });
 
-  it('by returning the promise after .then');
+  it('success by returning the promise after .then', () => {
+    return lineCount('server/06-asynch-callbacks/line-count.js')
+    .then((numberOfLines) => {
+      numberOfLines.should.be.equal(13);
+    });
+  });
 
-  it('by using eventually or fulfilledWith');
+  it('failure by returning the promise after .catch', () => {
+      return lineCount('is-not-there.js')
+      .catch((error) => {
+        error.message.should.be.equal('problem reading file: is-not-there.js');
+      });
+  });
+
+  it('success by using eventually or fulfilledWith', () => {
+      return lineCount('server/06-asynch-callbacks/line-count.js').should.be.eventually.equal(13);
+  });
+
+  it('failure by using rejectedWith', () => {
+    return lineCount('is-not-there.js').should.be.rejectedWith('problem reading file: is-not-there.js');
+  });
 });
