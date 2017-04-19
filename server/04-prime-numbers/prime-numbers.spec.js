214bdeac8bcc98a0f6f07f1038e5eaafe7528527
Matt Oberlin <moberlin8@gmail.com>
2017-04-19 12:17:44 -0400

    First test pass
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,24 @@
-describe('the prime numbers canary spec', () => {
+describe.only('the prime numbers canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  function primeFactors() {
+    return '';
+  }
+
+  describe('a prime refactor function should', () => {
+    it('1 should return nothing', () => {
+      primeFactors().should.be.equal('');
+    });
+    it('2 should return 2');
+    it('3 should return 3');
+    it('4 should return 2, 2');
+    it('5 should return 5');
+    it('6 should return 2, 3');
+    it('7 should return 7');
+    it('8 should return 2, 2, 2');
+    it('9 should return 3, 3, 3');
+    it('10 should return 2, 5');
   });
 });
