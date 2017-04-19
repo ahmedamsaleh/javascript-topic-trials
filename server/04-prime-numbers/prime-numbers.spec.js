2dea10f598251d2c625e1d87fae1aaa42e16451e
Matt Oberlin <moberlin8@gmail.com>
2017-04-19 13:08:02 -0400

    Still passes 8 tests, very cool, changed while to a for.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -6,9 +6,8 @@ describe.only('the prime numbers canary spec', () => {
   let primeFactors = (number) => {
     let factors = [];
     if (number > 1) {
-      while (number % 2 === 0){
+      for ( ; number % 2 === 0;  number /= 2 ){
         factors.push(2);
-        number /= 2;
       }
     }
     if (number > 1 ) {
